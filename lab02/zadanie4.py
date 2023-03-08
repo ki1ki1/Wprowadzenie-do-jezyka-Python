9fed1fa729667eed6572b6ae11a6585e6e896b3c
Wiktoriaaa <wgawronska@softfusion.pl>
2023-03-08 17:21:11 +0100

    fix

--- lab02/zadanie4.py
+++ lab02/zadanie4.py
@@ -1,4 +1,18 @@
+from datetime import datetime
 print('{:10.5}'.format("xylophone"))
+print(f'{"xylophone":10.5}')
+
 print('{:f}'.format(3.141592653589793))
-print('{:>10}'.format('test'))
-print('{:10}'.format('test'))
+print(f'{3.141592653589793:f}')
+
+print('{:{align}{width}}'.format('test', align='^', width='10'))
+print(f'{"test":{"^"}{10}}')
+
+print('{:>10}'.format("test"))
+print(f'{"test":>10}')
+
+print('{:10}'.format("test"))
+print(f'{"test":10}')
+
+print('{:%Y-%m-%d %H:%M}'.format(datetime.now()))
+print(f'{datetime.now():%Y-%m-%d %H:%M}')
